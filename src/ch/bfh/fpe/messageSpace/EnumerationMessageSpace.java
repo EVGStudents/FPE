d9d0a110924eef66a5e89c00ae55b63e85d8c543
Rizja Schmid <rizja.schmid@gmail.com>
2015-02-23 16:04:09 +0100

    EnumerationMessageSpace: improved performance of rank function

--- src/ch/bfh/fpe/messageSpace/EnumerationMessageSpace.java
+++ src/ch/bfh/fpe/messageSpace/EnumerationMessageSpace.java
@@ -2,47 +2,53 @@ package ch.bfh.fpe.messageSpace;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 /**
- * Message space that is defined over an enumeration.
- * The elements of the enumeration can be of arbitrary type M and are passed
+ * Message space that is defined over an enumList.
+ * The elements of the enumList can be of arbitrary type M and are passed
  * at construction time. Later they can not be changed to retain the order.
  * The user of this class has precise control over the position of each
  * element in the list before construction and is responsible to retain this order
  * for every subsequent use.
- * @param <M> type of the elements of the enumeration
+ * @param <M> type of the elements of the enumList
  */
 public class EnumerationMessageSpace<M> implements MessageSpace<M> {
 	
-	private final List<M> enumeration = new ArrayList<M>(); //Elements of the message space
+	private final List<M> enumList = new ArrayList<M>(); //Elements of the message space
+	private final Map<M,BigInteger> enumMap = new HashMap<M,BigInteger>(); //Elements with position index for fast rank
 	
 	/**
-	 * Constructs a enumeration messages space.
-	 * The enumeration must not be empty.
+	 * Constructs a enumList messages space.
+	 * The enumList must not be empty.
 	 * It is then copied to prevent modifications and retain the order.
-	 * @param enumeration List with elements of the message space.
+	 * @param enumList List with elements of the message space.
 	 */
 	public EnumerationMessageSpace(List<M> enumeration) {
 		if (enumeration == null || enumeration.size() == 0)
 			throw new IllegalArgumentException("Message space must not be empty.");
-		Set<M> tempSet = new HashSet<M>();
+		BigInteger i = BigInteger.ZERO;
 		for (M element : enumeration) {
-			if (tempSet.add(element))
-				this.enumeration.add(element);
+			if (!enumMap.containsKey(element)) {
+				this.enumMap.put(element, i);
+				this.enumList.add(element);
+				i = i.add(BigInteger.ONE);
+			}
 		}
 	}
 
 	/**
 	 * Returns the order of this message space,
-	 * therefore the number of elements in the enumeration
+	 * therefore the number of elements in the enumList
 	 * @return the order of the message space
 	 */
 	@Override
 	public BigInteger getOrder() {
-		return BigInteger.valueOf(enumeration.size());
+		return BigInteger.valueOf(enumList.size());
 	}
 
 	/**
@@ -53,9 +59,9 @@ public class EnumerationMessageSpace<M> implements MessageSpace<M> {
 	 */
 	@Override
 	public BigInteger rank(M value) {
-		int rank = enumeration.indexOf(value);
-		if (rank == -1) throw new OutsideMessageSpaceException("Value " + value);
-		return BigInteger.valueOf(rank);
+		BigInteger rank = enumMap.get(value);
+		if (rank == null) throw new OutsideMessageSpaceException("Value " + value);
+		return rank;
 	}
 
 	/**
@@ -68,7 +74,7 @@ public class EnumerationMessageSpace<M> implements MessageSpace<M> {
 	@Override
 	public M unrank(BigInteger rank) {
 		try {
-			return enumeration.get(rank.intValueExact());
+			return enumList.get(rank.intValueExact());
 		}
 		catch (IndexOutOfBoundsException ex) {
 			throw new OutsideMessageSpaceException("Rank " + rank);
